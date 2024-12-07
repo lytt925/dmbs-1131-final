c7b4b4ea7de19397cb435e391127bcb2d8174786
Chia-Chi-Shen <r12227108@ntu.edu.tw>
2024-12-07 14:48:53 +0800

    feat: user-apply

---
+++ client/app/user/apply.py
@@ -0,0 +1,25 @@
+import requests
+from config import API_BASE_URL
+
+def apply(email: str):
+    user_id = requests.get(f"{API_BASE_URL}/users", params={"email": email}).json()['data'][0]['id']
+
+    print("Please select a shelter:")
+    response = requests.get(f"{API_BASE_URL}/shelters")
+    shelters = response.json()['data']
+    for i, shelter in enumerate(shelters):
+        print(f"{i + 1}. {shelter['name']}")
+    shelter_id = int(input("Enter your choice: "))
+    shelter_id = shelters[shelter_id - 1]['id']
+
+    response = requests.get(f"{API_BASE_URL}/animals/unadopted", params={"shelter_id": shelter_id})
+    animals = response.json()
+    for i, animal in enumerate(animals):
+        print(f"{i + 1}. {animal['name']}\n | 品種: {animal['species']}\n | 性別: {animal['sex']}\n | 體型: {animal['size']}")
+        print("\n\n")
+
+    animal_id = int(input("Enter the animal ID you want to apply for: "))
+    print("Applying for adoption...")
+    animal_id = animals[animal_id - 1]['animal_id']
+    response = requests.post(f"{API_BASE_URL}/applications", json={"user_id": user_id, "animal_id": animal_id})
+    print("Application submitted successfully!\nPlease wait for the shelter to review your application.")
